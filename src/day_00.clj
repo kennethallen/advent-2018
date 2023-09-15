3a60280a25059ea48dc207dc4c56b9fe869905ba
Kenneth Allen <kenneth@kallen.dev>
2023-09-15 16:59:02 +1000

    Solve day 01 part 2

--- src/day_00.clj
+++ src/day_00.clj
@@ -1,6 +1,21 @@
 (ns day-00)
 
+(defn parse [l] (Long/parseLong l))
+
 (defn part-1 [lines]
   (->> lines
-    (map #(Long/parseLong %))
+    (map parse)
     (reduce +)))
+
+(defn part-2' [changes total history]
+  (let [history (conj history total)
+        total (+ total (first changes))]
+    (if (contains? history total)
+      total
+      (recur (rest changes) total history))))
+
+(defn part-2 [lines]
+  (part-2'
+    (cycle (map parse lines))
+    0
+    #{}))
--- test/day_00_test.clj
+++ test/day_00_test.clj
@@ -8,3 +8,11 @@
   (is (= -6 (part-1 ["-1" "-2" "-3"])))
   (with-open [rdr (clojure.java.io/reader "test/input/00.txt")]
     (is (= 505 (part-1 (line-seq rdr))))))
+
+(deftest test-part-2
+  (is (= 0 (part-2 ["+1" "-1"])))
+  (is (= 10 (part-2 ["+3" "+3" "+4" "-2" "-4"])))
+  (is (= 5 (part-2 ["-6" "+3" "+8" "+5" "-6"])))
+  (is (= 14 (part-2 ["+7" "+7" "-2" "-7" "-4"])))
+  (with-open [rdr (clojure.java.io/reader "test/input/00.txt")]
+    (is (= 72330 (part-2 (line-seq rdr))))))
