ea2612fac2de2ac74b6cea93cf1eb43e7a6a3cbf
Kenneth Allen <kenneth@kallen.dev>
2023-09-17 17:36:13 +1000

    Solve day 04 part 1

---
+++ src/day_04.clj
@@ -0,0 +1,40 @@
+(ns day-04
+  (:require [clojure.instant :refer [read-instant-date]]))
+
+(defrecord Record [ts event]) ; Event is :sleep, :wake, or the ID of a guard beginning shift
+
+(def pattern #"\[(\d{4}-\d\d-\d\d) (\d\d:\d\d)\] (Guard #(\d+) begins shift|falls asleep|wakes up)")
+(defn parse [l]
+  (let [[_ date time event id] (re-matches pattern l)]
+    (Record.
+      (read-instant-date (format "%sT%s" date time))
+      (cond
+        (= event "falls asleep") :sleep
+        (= event "wakes up") :wake
+        :else (Long/parseLong id)))))
+
+(defn guard-minute-sleeps [records]
+  (get
+    (reduce
+      (fn [[guard asleep-since data] rec]
+        (cond
+          (= (:event rec) :sleep) [guard (.getMinutes (:ts rec)) data]
+          (= (:event rec) :wake) [guard nil
+            (reduce
+              (fn [data minute]
+                (assoc data guard
+                  (assoc (data guard {}) minute
+                    (inc ((data guard {}) minute 0)))))
+              data
+              (range asleep-since (.getMinutes (:ts rec))))]
+          :else [(:event rec) nil data]))
+      [nil nil {}]
+      records)
+    2))
+
+(defn part-1 [lines]
+  (let [records (sort-by :ts (map parse lines))
+        gms (guard-minute-sleeps records)
+        guard (key (apply max-key #(reduce + (vals (val %))) gms))
+        minute (key (apply max-key val (gms guard)))]
+    (* guard minute)))
