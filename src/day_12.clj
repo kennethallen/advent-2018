c9e12327a5addb2d33977e020a7ddd6e2af44e9f
Kenneth Allen <kenneth@kallen.dev>
2023-10-02 15:50:25 +1100

    Solve day 12 part 2

--- src/day_12.clj
+++ src/day_12.clj
@@ -7,26 +7,44 @@
 (defn parse-spec [spec]
   (reduce bit-set 0 (plant-idxs spec)))
 
+(defn normalize [old-base state]
+  (let [state (dense-int-set state)
+        base (apply min state)]
+    [(+ base old-base) (dense-int-set (map #(- % base) state))]))
+
 (defn parse [lines]
   (let [[_ init] (re-matches #"initial state: ([#\.]*)" (first lines))
-        init (dense-int-set (plant-idxs init))
+        init (normalize 0 (plant-idxs init))
         rules (map #(re-matches #"([#\.]{5}) => ([#\.])" %) (nnext lines))
         passes (dense-int-set (map parse-spec (map second (filter #(= "#" (get % 2)) rules))))]
     (assert (not (contains? passes 2r00000)))
     [init passes]))
 
-(defn step [state passes]
-  (dense-int-set
-    (filter
-      (fn [n] (contains? passes (reduce bit-set 0 (filter #(contains? state (+ % n -2)) (range 5)))))
-      (range (- (apply min state) 2) (+ (apply max state) 2)))))
+(defn step [[base state] passes]
+  (if (empty? state)
+    (dense-int-set)
+    (normalize
+      base
+      (filter
+        (fn [n] (contains? passes (reduce bit-set 0 (filter #(contains? state (+ % n -2)) (range 5)))))
+        (range (- (apply min state) 2) (+ (apply max state) 2))))))
 
-(defn debug-view [state]
-  [(apply min state) (apply str (map #(if (contains? state %) \# \.) (range (apply min state) (inc (apply max state)))))])
+(defn split-at' [i v] [(subvec v 0 i) (subvec v i)])
 
-(defn solve [lines gens]
+(defn solve' [gens states passes]
+  (let [[next-base next-state] (step (last gens) passes)]
+    (if-some [match-idx (get states next-state)]
+      (conj (split-at' match-idx gens) (- next-base (first (get gens match-idx))))
+      (recur (conj gens [next-base next-state]) (assoc states next-state (count gens)) passes))))
+(defn solve [lines gen-idx]
   (let [[init passes] (parse lines)
-        state (nth (iterate #(step % passes) init) gens)]
-    (reduce + state)))
+        [prefix cycle cycle-shift] (solve' [init] {init 0} passes)]
+    (if-some [[base state] (get prefix gen-idx)]
+      (+ (* (count state) base) (reduce + state))
+      (let [gen-idx-after-prefix (- gen-idx (count prefix))
+            cycle-count (quot gen-idx-after-prefix (count cycle))
+            [base state] (get cycle (mod gen-idx-after-prefix (count cycle)))]
+        (+ (* (count state) (+ base (* cycle-count cycle-shift))) (reduce + state))))))
 
 (def part-1 #(solve % 20))
+(def part-2 #(solve % 50000000000))
--- test/day_12_test.clj
+++ test/day_12_test.clj
@@ -1,6 +1,6 @@
 (ns day-12-test
   (:require [clojure.test :refer :all]
-            [day-12 :refer [part-1]]))
+            [day-12 :refer [part-1 part-2]]))
 
 (def sample [
   "initial state: #..#.#..##......###...###"
@@ -24,3 +24,7 @@
   (is (= 325 (part-1 sample)))
   (with-open [rdr (clojure.java.io/reader "test/input/12.txt")]
     (is (= 3725 (part-1 (line-seq rdr))))))
+
+(deftest test-part-2
+  (with-open [rdr (clojure.java.io/reader "test/input/12.txt")]
+    (is (= 3100000000293 (part-2 (line-seq rdr))))))
